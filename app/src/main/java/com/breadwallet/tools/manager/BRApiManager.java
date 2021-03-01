c9b00503743e7bfcc3e457c388df52aca11d8dba
vcexnet <36216930+vcexnet@users.noreply.github.com>
2021-03-01 23:59:22 +0800

    update exchange rates!

--- app/src/main/java/com/breadwallet/tools/manager/BRApiManager.java
+++ app/src/main/java/com/breadwallet/tools/manager/BRApiManager.java
@@ -157,7 +157,7 @@ public class BRApiManager {
 
 
     public static JSONArray fetchRates(Activity activity) {
-        String jsonString = urlGET(activity, "https://api.loshan.co.uk/api/v1/rates");
+        String jsonString = urlGET(activity, "https://api.eactalk.com/api/v1/rates/");
         JSONArray jsonArray = null;
         if (jsonString == null) return null;
         try {
@@ -169,7 +169,7 @@ public class BRApiManager {
     }
 
     public static JSONArray backupFetchRates(Activity activity) {
-        String jsonString = urlGET(activity, "https://api.loafwallet.org/api/v1/rates");
+        String jsonString = urlGET(activity, "https://exapi.eacpay.com/api/v1/rates");
 
         JSONArray jsonArray = null;
         if (jsonString == null) return null;
